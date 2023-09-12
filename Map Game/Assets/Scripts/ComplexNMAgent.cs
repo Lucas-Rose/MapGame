5792b378ba7520fa5d98ed4a85af224e23e58982
Lucas Rose <107737308+Lucas-Rose@users.noreply.github.com>
2023-09-13 00:13:13 +1000

    BugFix Pathfinder

--- Map Game/Assets/Scripts/ComplexNMAgent.cs
+++ Map Game/Assets/Scripts/ComplexNMAgent.cs
@@ -11,17 +11,20 @@ public class ComplexNMAgent : MonoBehaviour
     [SerializeField] private Transform indicatorContainer;
     [SerializeField] private GameObject indicator;
     [SerializeField] private GameObject offsetIndicator;
+    [SerializeField] private GameObject goalIndicator;
 
     [Header("Settings")]
     [SerializeField] private float waypointSensitivity;
     [SerializeField] private Vector2 xOffset;
     [SerializeField] private Vector2 zOffset;
 
+    private List<Vector3> locationGoals;
     private List<Vector3> waypoints;
     private NavMeshAgent agent;
 
     private void Start()
     {
+        locationGoals = new List<Vector3>();
         waypoints = new List<Vector3>();
         agent = GetComponent<NavMeshAgent>();
         agent.updateRotation = false;
@@ -31,23 +34,35 @@ public class ComplexNMAgent : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            GenerateWaypoints();
-            DrawWaypoints(false);
-            OffSetWayPoints();
-            DrawWaypoints(true);
+            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                AddLocationGoal(hit.point);
+            }
         }
         if(waypoints.Count > 0)
         {
-            agent.SetDestination(waypoints[0]);
+            if (!agent.hasPath && !agent.pathPending)
+            {
+                agent.SetDestination(waypoints[0]);
+            }
+            
             if (Vector3.Distance(transform.position, waypoints[0]) <= waypointSensitivity)
             {
-                Debug.Log("removing waypoint");
                 waypoints.RemoveAt(0);
             }
         }
-       
-    }
 
+    }
+    private void AddLocationGoal(Vector3 goalLocation)
+    {
+        locationGoals.Add(goalLocation);
+        AddToWaypoints(goalLocation);
+        OffSetWayPoints();
+        Instantiate(goalIndicator, goalLocation, Quaternion.identity, indicatorContainer);
+    }
     private void GenerateWaypoints()
     {
         waypoints = new List<Vector3>();
@@ -73,14 +88,10 @@ public class ComplexNMAgent : MonoBehaviour
         {
             waypoints.Add(i);
         }
+        agent.isStopped = false;
     }
     private void DrawWaypoints(bool offset)
-    {
-        //Clear indicators
-        for(int i = 0; i < indicatorContainer.childCount; i++)
-        {
-            Destroy(indicatorContainer.GetChild(i).gameObject);
-        }
+    {        
         foreach(Vector3 i in waypoints)
         {
             if (offset)
@@ -101,6 +112,10 @@ public class ComplexNMAgent : MonoBehaviour
             float xPush = Random.Range(xOffset.x, xOffset.y);
             float zPush = Random.Range(zOffset.x, zOffset.y);
             waypoints[i] = new Vector3(waypoints[i].x + xPush, waypoints[i].y, waypoints[i].z + zPush);
+        }
+        for(int i = 0; i < waypoints.Count -1; i++)
+        {
+            Debug.DrawLine(waypoints[i], waypoints[i + 1]);
         }
     }
 }
